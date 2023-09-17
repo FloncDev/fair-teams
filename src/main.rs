1bec6bae366e039531af62a97edc6cb4e39bbd25
DevFlock <37045958+DevFlock@users.noreply.github.com>
2023-09-17 19:12:20 +0100

    ✨ Add function to read json file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
-use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::{io, collections::HashMap};
+use std::{fs, collections::HashMap};
 
-fn read_json() -> HashMap<f32> {
-    let file = io::read_to_string("rankings.json");
+fn read_json() -> HashMap<String, f64> {
+    let file = fs::read_to_string("rankings.json").expect("Could not find file.");
+
+    let json: Value = serde_json::from_str(file.as_str()).expect("Json not well formatted");
+    let raw_rankings = json.as_object().unwrap();
+
+    let mut rankings: HashMap<String, f64> = HashMap::new();
+
+    for (name, rank) in raw_rankings {
+        let rank: f64 = rank.as_f64().unwrap();
+        let name = name.to_owned();
+
+        rankings.insert(name, rank);
+    } 
+
+    rankings
+}
+
+struct Team {
+    players: Vec<String>,
+    ranking: f64
 }
 
 fn main() {
-    println!("Hello, world!");
+    let rankings = read_json();
+    let mut teams: Vec<Team> = vec![];
+
+    // https://softwareengineering.stackexchange.com/a/339534
 }
