76cb57ebb2dbc536c442dc724d9e0c6541019a2b
DevFlock <37045958+DevFlock@users.noreply.github.com>
2023-09-19 22:04:39 +0100

    🗃️ Update models to support mongo

--- src/teams/models.rs
+++ src/teams/models.rs
@@ -1,7 +1,22 @@
+use mongodb::bson::oid::ObjectId;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Player {
-    pub id: String,
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
     pub name: String,
+    #[serde(skip_serializing)]
     pub skill: f64
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Rating {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub rater_id: ObjectId,
+    pub ratee_id: ObjectId,
+    pub rating: f64
 }
 
 pub struct Team {
