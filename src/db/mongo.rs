445036d49f31f5964c4f42a93fd18c16173414bc
DevFlock <37045958+DevFlock@users.noreply.github.com>
2023-09-25 23:06:09 +0100

    💚 Let id live longer

--- src/db/mongo.rs
+++ src/db/mongo.rs
@@ -98,8 +98,9 @@ impl Mongo {
 
     pub async fn get_player_by_name(&self, name: String) -> Result<Player, Error> {
         let id = self.players.find(doc! {"name": name}, None).await.unwrap();
+        let id = id.current();
 
-        self.get_player(id.current().get_object_id("_id").unwrap()).await
+        self.get_player(id.get_object_id("_id").unwrap()).await
     }
 
     pub async fn get_players(&self) -> Vec<Player> {
